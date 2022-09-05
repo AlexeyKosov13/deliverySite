af2cde22a587656f5b8f5290ef55aeba3dfa1e5c
AlexeyKosov <foster-13@yandex.ru>
2022-09-05 22:17:36 +0300

    add logic cart

--- deliverySite/js/main.js
+++ deliverySite/js/main.js
@@ -144,7 +144,7 @@
 
 (function () {
   getProducts();
-  let productsArray=[];
+  let productsArray = [];
 
   const productsContainer = document.querySelector(".products__items");
 
@@ -155,11 +155,13 @@
   }
 
   const removeChild = (item) => {
-    while(item.firstChild) {
+    while (item.firstChild) {
       item.removeChild(item.firstChild);
-    };
+    }
   };
 
+
+  //================renderProducts================
   const renderProducts = (container, productsArray) => {
     removeChild(container);
     productsArray.forEach(function (item) {
@@ -192,11 +194,12 @@
     `;
       container.insertAdjacentHTML("beforeend", productHTML);
     });
-  }
+  };
 
   const productsList = document.querySelector(".products__list");
-  
 
+
+  //=====filterProducts==================
   productsList.addEventListener("click", (e) => {
     const target = e.target;
     const productsItems = JSON.parse(JSON.stringify(productsArray));
@@ -238,36 +241,29 @@
       let currentItem = productsItems[i];
       if (currentItem.data === filterValue) {
         filterdItems.push(currentItem);
-      };
-    };
+      }
+    }
     renderProducts(productsContainer, filterdItems);
-    
   });
-})();
 
-//================logic=========================
-
-// const burger = document.querySelector(".burger");
 const order = document.querySelector(".order");
-// const close = document.querySelector(".menu__close");
-// const closeOrder = document.querySelector(".menuOrder__close");
-// const menu = document.querySelector(".menu");
-// const popup = document.querySelector(".popup");
-// const menuOrder = document.querySelector(".menu__order");
 const orderTotal = document.querySelector(".order__total-price");
 const cartWrapper = document.querySelector(".cart-wrapper");
 const orderWrapper = document.querySelector(".order__list");
-// const orderBtn = document.querySelector(".order__btn");
-// const productsContainer = document.querySelector(".products__items");
+  const orderBtn = document.querySelector(".order__btn");
+  const menuOrder = document.querySelector(".menu__order");
+  const popup = document.querySelector(".popup");
 
 let cart = [];
 
-order.addEventListener("click", () => {
+//=========open cart popup===============
+  order.addEventListener("click", () => {
+    removeChild(orderWrapper);
   cart.forEach((item) => {
     const cartItemHTML = `
             <div class="cart-item order__item" data-category="${item.data}" data-id="${item.id}">
                     <div class="cart-item__img order__img">
-                        <img src="${item.imgSrc}" alt="${item.title}">
+                        <img src="../img/roll/${item.imgSrc}" alt="${item.title}">
                     </div>
                     <div class="cart-item__desc order__descr">
                         <div class="cart-item__title">${item.title}</div>
@@ -279,43 +275,48 @@ order.addEventListener("click", () => {
                             </div>
                         </div>
                     </div>
-                  <div class="order__total"> x ${item.counter}</div>
+                  <div class="order__total"> x ${item.itemsInCart}</div>
             </div>`;
     orderWrapper.insertAdjacentHTML("afterbegin", cartItemHTML);
   });
-});
+  });
+  
+  document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.target.reset(); 
+    menuOrder.classList.remove("is--active");
+    popup.classList.add("is--active");
+    cartWrapper.innerHTML = "";
+    cart.length = 0;
+    calcCartPrice();
+  });
 
 
-function toggleCartStatus() {
-  const cartWrapper = document.querySelector(".products__cart");
-  const cartEmptyBadge = document.querySelector("[data-cart-empty]");
-  const orderForm = document.querySelector("#order-form");
+//=============empty cart toggle============
+  const toggleCartStatus = () => {
+    const cartWrapper = document.querySelector(".products__cart");
+    const cartEmptyBadge = document.querySelector("[data-cart-empty]");
+    const orderForm = document.querySelector("#order-form");
 
-  if (cartWrapper.children.length > 0) {
-    cartEmptyBadge.classList.add("none");
-    orderForm.classList.remove("none");
-  } else {
-    cartEmptyBadge.classList.remove("none");
-    orderForm.classList.add("none");
-  }
-}
+    if (cartWrapper.children.length > 0) {
+      cartEmptyBadge.classList.add("none");
+      orderForm.classList.remove("none");
+    } else {
+      cartEmptyBadge.classList.remove("none");
+      orderForm.classList.add("none");
+    };
+  };
 
 // пересчет общей стоимости товаров в корзине
 function calcCartPrice() {
   const totalWrapper = document.querySelector(".total-price");
-  const cartItems = document.querySelectorAll(".cart-item");
   const deliveryCost = document.querySelector(".delivery-cost");
   const cartDelivery = document.querySelector("[data-cart-delivery]");
 
   let totalPrice = 0;
 
-  cartItems.forEach(function (item) {
-    const amountEl = item.querySelector("[data-counter]");
-    const priceEl = item.querySelector(".price__currency");
-    const currentPrice =
-      parseInt(amountEl.innerText) * parseInt(priceEl.innerText);
-    totalPrice += currentPrice;
-  });
+  totalPrice = cart.reduce((sum, item) => sum + (item.price*item.itemsInCart), 0);
+ 
   // отображаем цену на страницу
   totalWrapper.innerText = totalPrice;
   orderTotal.innerText = `Итого: ${totalPrice} руб.`;
@@ -325,7 +326,7 @@ function calcCartPrice() {
     cartDelivery.classList.remove("none");
   } else {
     cartDelivery.classList.add("none");
-  }
+  };
 
   if (totalPrice >= 600) {
     deliveryCost.classList.add("free");
@@ -333,18 +334,16 @@ function calcCartPrice() {
   } else {
     deliveryCost.classList.remove("free");
     deliveryCost.innerText = "250 ₽";
-  }
-}
+  };
+};
 
 //добавление или уменьшение количества в корзине
-
 window.addEventListener("click", function (event) {
-  
   let counter;
   let productId;
   if (event.target.closest(".cart-wrapper")) {
-    productId = event.target.closest(".cart-item").dataset.id;
-  };
+    productId = +(event.target.closest(".cart-item").dataset.id);
+  }
 
   if (
     event.target.dataset.action === "plus" ||
@@ -352,28 +351,27 @@ window.addEventListener("click", function (event) {
   ) {
     //находим обертку счетчика
     const counterWrapper = event.target.closest(".counter-wrapper");
-    // находим див с числом счетчика
     counter = counterWrapper.querySelector("[data-counter]");
-  };
+  }
 
   if (event.target.dataset.action === "plus") {
     counter.innerText = ++counter.innerText;
     cart.map((item) => {
       if (item.id === productId) {
-        item.counter++;
-      }
+        item.itemsInCart++;
+        calcCartPrice();
+      };
     });
   };
   // проверка что клик был совершен по кнопке минус
   if (event.target.dataset.action === "minus") {
-    // проверяем чтобы счетчик был больше 1
     if (parseInt(counter.innerText) > 1) {
-      //изменяем текст в счетчике уменьшаем на 1
       counter.innerText = --counter.innerText;
       cart.map((item) => {
         if (item.id === productId) {
-          item.counter--;
-        }
+          item.itemsInCart--;
+          calcCartPrice();
+        };
       });
     } else if (
       event.target.closest(".cart-wrapper") &&
@@ -383,14 +381,14 @@ window.addEventListener("click", function (event) {
       cart.map((item, index) => {
         if (item.id === productId) {
           cart.splice(index, 1);
-        }
+        };
       });
 
       toggleCartStatus();
       // пересчет общей стоимости товаров в корзине
       calcCartPrice();
-    }
-  }
+    };
+  };
 
   if (
     event.target.hasAttribute("data-action") &&
@@ -398,39 +396,26 @@ window.addEventListener("click", function (event) {
   ) {
     // пересчет общей стоимости товаров в корзине
     calcCartPrice();
-  }
+  };
 });
-
+  
+//============add to cart =============
 window.addEventListener("click", function (event) {
   // проверяем что  клик по кнопке добавить в карзину
   if (event.target.hasAttribute("data-cart")) {
-    //находим карточку товара по которй был клик
-    const card = event.target.closest(".card");
-    //собираем данные с карточки
-    const productInfo = {
-      id: card.dataset.id,
-      imgSrc: card.querySelector(".product-img").getAttribute("src"),
-      title: card.querySelector(".item-title").innerText,
-      itemsInBox: card.querySelector("[data-items-in-box]").innerText,
-      weight: card.querySelector(".price__weight").innerText,
-      price: card.querySelector(".price__currency").innerText,
-      counter: card.querySelector("[data-counter]").innerText,
-    };
-
-    cart.push(productInfo);
-
-    // проверяем есть ли такой товар в корзине
-    const itemInCart = cartWrapper.querySelector(
-      `[data-id="${productInfo.id}"]`
-    );
-
-   const renderCart = (item) => {
+    //находим карточку товара по которой был клик
+    const card = event.target.closest(".card");  
+    const id = +(card.dataset.id);
+    const counter = +(card.querySelector("[data-counter]").innerText);
     
-       const cartItemHTML = `
+    const itemInCart = cartWrapper.querySelector(`[data-id="${id}"]`);
+    
+    const renderCart = (item) => {
+      const cartItemHTML = `
             <div class="cart-item" data-id="${item.id}">
                 <div class="cart-item__top">
                     <div class="cart-item__img">
-                        <img src="${item.imgSrc}" alt="${item.title}">
+                        <img src="../img/roll/${item.imgSrc}" alt="${item.title}">
                     </div>
                     <div class="cart-item__desc">
                         <div class="cart-item__title">${item.title}</div>
@@ -439,7 +424,7 @@ window.addEventListener("click", function (event) {
                         <div class="cart-item__details">
                             <div class="items items--small counter-wrapper">
                                 <div class="items__control" data-action="minus">-</div>
-                                <div class="items__current" data-counter="">${item.counter}</div>
+                                <div class="items__current" data-counter="">${item.itemsInCart}</div>
                                 <div class="items__control" data-action="plus">+</div>
                             </div>
 
@@ -451,66 +436,84 @@ window.addEventListener("click", function (event) {
                 </div>
             </div>`;
       cartWrapper.insertAdjacentHTML("beforeend", cartItemHTML);
+    };
     
-   };
-
     // если товар в корзине плюсуем количество
     if (itemInCart) {
       const counterEl = itemInCart.querySelector("[data-counter]");
-      counterEl.innerText =
-        parseInt(counterEl.innerText) + parseInt(productInfo.counter);
+      counterEl.innerText = parseInt(counterEl.innerText) + parseInt(counter);
+      cart.map((item) => {
+        if (item.id === id) {
+          item.itemsInCart = parseInt(counterEl.innerText); 
+          calcCartPrice();
+        };
+      });
     } else {
       //если товара нет в корзине
-      renderCart(productInfo);
-    }
-    //сброс счетчика на 1
-    card.querySelector("[data-counter]").innerText = "1";
-    // отображение статуса корзины
-    toggleCartStatus();
+      productsArray.map((item) => { 
+      if (item.id === id) {      
+        item.itemsInCart = +(counter);
+        cart.push(item);
+        renderCart(item);
+        toggleCartStatus();
+        calcCartPrice();
+      };
+    });
+    };
 
-    // пересчет общей стоимости товаров в корзине
+    card.querySelector("[data-counter]").innerText = "1";
+    toggleCartStatus(); 
     calcCartPrice();
-  }
+  };
 });
 
-//=======yadex_maps=========
+})();
 
-//============ geoObject ==================
 
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [56.141051, 47.194952],
-    zoom: 12,
-  });
 
-  var address = document.querySelector("#myadress").innerHTML;
+//============ yandexMap ==================
+(function () {
+  if (typeof ymaps === "undefined") {
+    return;
+  };
 
-  var geocoder = ymaps.geocode(address);
-
-  geocoder.then(function (res) {
-    var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
-    myMap.setCenter(coordinates, 14, {
-      checkZoomRange: true,
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [56.141051, 47.194952],
+      zoom: 12,
     });
-    var placemark = new ymaps.Placemark(
-      coordinates,
-      {
-        hintContent: address,
-        balloonContent: "Время работы: Пн-Пт, с 9 до 20",
-      },
-      {
-        iconLayout: "default#image",
-        iconImageHref: "../img/icon.svg",
-        iconImageSize: [40, 52],
-        iconImageOffset: [-5, -38],
-      },
-      {
-        preset: "islands#redDotIcon",
-      }
-    );
 
-    myMap.geoObjects.add(placemark);
-  });
-}
+    var address = document.querySelector("#myadress").innerHTML;
+
+    var geocoder = ymaps.geocode(address);
+
+    geocoder.then(function (res) {
+      var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+      myMap.setCenter(coordinates, 16, {
+        checkZoomRange: true,
+      });
+      var placemark = new ymaps.Placemark(
+        coordinates,
+        {
+          hintContent: address,
+          balloonContent: "Время работы: Пн-Пт, с 9 до 20",
+        },
+        {
+          iconLayout: "default#image",
+          iconImageHref: "../img/icon.svg",
+          iconImageSize: [40, 52],
+          iconImageOffset: [-5, -38],
+        },
+        {
+          preset: "islands#redDotIcon",
+        }
+      );
+
+      myMap.geoObjects.add(placemark);
+      myMap.behaviors.disable("scrollZoom");
+    });
+  }
+})();
+
 //====================/yandexMaps=======================
